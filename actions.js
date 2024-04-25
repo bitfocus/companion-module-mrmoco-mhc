b07ced6b3ac65d485762ccdf6bb79420b92850b0
Mihindu Paul <mihindu.paul@mrmoco.com>
2024-04-25 12:03:29 +0100

    first commit

--- actions.js
+++ actions.js
@@ -1,20 +1,36 @@
 module.exports = function (self) {
+
 	self.setActionDefinitions({
 		sample_action: {
-			name: 'My First Action',
+			name: 'Recall Preset',
 			options: [
+				{
+					id: 'robot',
+					type: 'number',
+					label: 'Robot ID',
+					default: 1,
+					min: 1,
+					max: 100,
+				},
 				{
 					id: 'num',
 					type: 'number',
-					label: 'Test',
-					default: 5,
+					label: 'Preset ID',
+					default: 0,
 					min: 0,
 					max: 100,
 				},
 			],
 			callback: async (event) => {
-				console.log('Hello world!', event.options.num)
+				const url = `/v1/robots/${event.options.robot}/preset/${event.options.num}`
+				self.sendCam(self, url)
 			},
 		},
+		start_preset: {
+			name: 'Stop All',
+			callback: async (event) => {
+				self.sendCam(self, '/v1/robots/stop')
+			},
+		}
 	})
 }
--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const UpdateActions = require('./actions')
 const UpdateFeedbacks = require('./feedbacks')
 const UpdateVariableDefinitions = require('./variables')
 
+
+
 class ModuleInstance extends InstanceBase {
+
 	constructor(internal) {
 		super(internal)
 	}
@@ -27,22 +30,78 @@ class ModuleInstance extends InstanceBase {
 		this.config = config
 	}
 
+	async sendCam(self, str) {
+		const { default: got } = await import('got');
+		if (str) {
+
+			const url = `https://${self.config.httpHost}:${self.config.httpPort}${str}`
+
+			if (self.config.debug) {
+				self.log('debug', `Sending : ${url}`)
+			}
+
+			try {
+				const response = await got.put(
+					url,
+					{
+						username:self.config.username,
+						password:self.config.password,
+						https: { rejectUnauthorized: false }
+					}
+				)
+				console.log("Result from REST:" + str);
+			} catch (err) {
+				throw new Error(`Action failed err: ${err}`)
+			}
+		}
+	}
+
+	async getRobotList(self) {
+		const { default: got } = await import('got');
+		try {
+			const response = await got.get(
+				`https://${self.config.httpHost}:${self.config.httpPort}/v1/status`,
+				{
+					username:self.config.username,
+					password:self.config.password,
+					https: { rejectUnauthorized: false }
+				}
+			).json()
+			console.log("Result from REST:" + response);
+		} catch (err) {
+			return []
+		}
+	}
+
 	// Return config fields for web config
 	getConfigFields() {
 		return [
 			{
 				type: 'textinput',
-				id: 'host',
+				id: 'httpHost',
 				label: 'Target IP',
 				width: 8,
 				regex: Regex.IP,
 			},
 			{
 				type: 'textinput',
-				id: 'port',
+				id: 'httpPort',
 				label: 'Target Port',
 				width: 4,
 				regex: Regex.PORT,
+			},
+			{
+				type: 'textinput',
+				id: 'username',
+				label: 'User Name',
+				width: 4,
+				value: 'operator',
+			},
+			{
+				type: 'textinput',
+				id: 'password',
+				label: 'Password',
+				width: 4,
 			},
 		]
 	}
